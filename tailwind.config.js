d51cef7f7cd3230df26140d9088ac9402fbeffbb
Sumit Kumar <bingostudypoint@gmail.com>
2025-11-03 13:17:49 +0530

    setup is done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./Admin/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./WebsiteInterface/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
